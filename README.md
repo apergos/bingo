baa18b831b0ea5d23ac108f2ee6d10aca7b9cd93
Ariel T. Glenn <ariel@wikimedia.org>
2019-01-11 01:12:54 +0200

    Make the script write the nice js-using html file
    README has been updated accordingly.

--- README.md
+++ README.md
@@ -1,41 +1,32 @@
 Bingo Card Generator
 ====================
-Use of bingo.html:
-Put the background image of your choice into bg-image.jpg, edit the title in the html file to whatever
-you like, add more or different trumpisms in the html file by extending the list 'trumpisms', and load
-up into your browser. Clicking on a square marks it for bingo. If you hit the wrong square you can always
+How to use this script:
+----------------------
+
+python3 bingo-generator.py --help will give you a usage message.
+
+Zeroth, save a background image of your choice with the name bg-image.jpg.
+
+First, create a list of short phrases to go in the bingo squares; you need at least 24 of them,
+one per line, in a file. See sample_terms.txt or trump_terms.txt for examples.
+
+Second, decide on the title for your bingo card.
+
+Third, decide what color you want the squares to turn when they are clicked; the default
+is an annoying shade of orange.
+
+Now run the script:
+
+```shell
+python3 bingo-generator.py -t path-to-term-list-file -o name-of-html-file-to-write -h title-of-card -c '#nnnnnn'
+```
+
+Load up the html file in a browser.
+
+Clicking on a square marks it for bingo. If you hit the wrong square you can always
 click it again to clear it. Nothing spectacular happens when you get bingo unless you jump up and shout
 BINGO! yourself and have a drink :-)
 
-Use of python script:
+Click 'new card' to have it clear any marked squares and reload terms in a different order.
 
-Make sure you put the background image of your choice into bg-image.jpg and edit template.html to use
-the title (in h1 tags) you prefer. Then run the script with python3.
-
-Once you've run the script and viewed the html in your browser, resize the browser and printscreen the
-area you want for the card, for a cheap quick image. I've repurposed it with the intent to produce one
-card, with title, for the web. If you wanted something else, the original fork is still there...
-
-Original text from source follows:
-
-Some terrible old code I wrote that belongs on the Internet.
-
-Just dump your potential bingo terms into a text file, each on its own line, run the program, and print out the resulting HTML. Pagebreaks, etc. *should* be handled gracefully.
-
-Usage:
-------
-
-```shell
-bingo-generator.py  [file of terms] [output file] [# of cards]
-```
-
-Example:
---------
-
-```shell
-python bingo-generator.py bingo_terms.txt bingo.html 20
-```
-
-
-[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/hrs/bingo/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
-
+This has drifted pretty far from the original author's intent. Yay open source? :-)
--- bingo-generator.py
+++ bingo-generator.py
@@ -1,57 +1,119 @@
 #!/usr/bin/env python3
-import random
 import sys
+import getopt
 
 
-def generate_table(terms, pagebreak=True):
-    '''Generates an HTML table representation of the bingo card for terms'''
-    terms_and_space = terms[:12] + ["FREE SPACE"] + terms[12:24]
-    if pagebreak:
-        res = "<table class=\"newpage\">\n"
-    else:
-        res = "<table>\n"
-    for i, term in enumerate(terms_and_space):
-        if i % 5 == 0:
-            res += "\t<tr>\n"
-        if i == 12:
-            # center square
-            res += "\t\t<td bgcolor=\"#ffcc99\">" + term + "</td>\n"
+def usage(message=None):
+    '''
+    display a nice usage message along with an optional message
+    describing an error
+    '''
+    if message:
+        sys.stderr.write(message + "\n")
+    usage_message = """Usage: python3 bingo-generator.py --terms <path> [--output <path>]
+       [--header <string>] [--color <#nnnnnn>]
+or: python3 bingo-generator.py --help
+
+Reads a list of phrases from a file, fills in an html template,
+and writes the result, possibly with a title, to stdout or
+optionally to a file.
+
+Arguments:
+
+ --terms   (-t):  path to file containing list of terms, one per line;
+                  there should be 24 of them at least
+ --output  (-o):  path to file into which to write the html
+                  Default: written to stdout
+ --heading (-h):  what to call the bingo card (goes in the <h1> header)
+                  Default: Bingo Card
+ --color   (-c):  color to turn a square when it's been clicked
+                  Default: #ffcc99
+ --help    (-H):  show this help message
+
+Example:
+
+python3 bingo-generator.py -t trump_terms.txt -o trump_bingo.html -h "Presidential Border Bingo"
+"""
+    sys.stderr.write(usage_message)
+    sys.exit(1)
+
+
+def get_default_opts():
+    '''
+    initialize args with default values and return them
+    '''
+    args = {'output': None, 'heading': 'Bingo Card', 'color': '#ffcc99'}
+    return args
+
+
+def process_opts():
+    '''
+    get command-line args and values, falling back to defaults
+    where needed, whining about bad args
+    '''
+    try:
+        (options, remainder) = getopt.gnu_getopt(
+            sys.argv[1:], "t:o:h:c:H", ["terms=", "output=", "heading=", 'color=', "help"])
+
+    except getopt.GetoptError as err:
+        usage("Unknown option specified: " + str(err))
+
+    args = get_default_opts()
+
+    for (opt, val) in options:
+        if opt in ["-t", "--terms"]:
+            args['terms'] = val
+        elif opt in ["-o", "--output"]:
+            args['output'] = val
+        elif opt in ["-h", "--heading"]:
+            args['heading'] = val
+        elif opt in ["-c", "--color"]:
+            args['color'] = val
+        elif opt in ["-H", "--help"]:
+            usage('Help for this script\n')
         else:
-            res += "\t\t<td>" + term + "</td>\n"
-        if i % 5 == 4:
-            res += "\t</tr>\n"
-    res += "</table>\n"
-    return res
+            usage("Unknown option specified: <%s>" % opt)
+
+    if remainder:
+        usage("Unknown option(s) specified: {opt}".format(opt=remainder[0]))
+
+    check_opts(args)
+    return args
+
+
+def check_opts(args):
+    '''
+    whine if mandatory args not supplied
+    '''
+    for name in ['terms']:
+        if name not in args or not args[name]:
+            usage("Mandatory argument {name} not specified".format(name=name))
 
 
 def do_main():
     '''entry point'''
 
-    # check for the right # of args
-    if len(sys.argv) != 4:
-        print("USAGE: " + sys.argv[0], " [file of terms] [output file] [# of cards]")
-        print("Example: " + sys.argv[0] + " bingo_terms.txt bingo.html 20")
-        sys.exit(1)
+    args = process_opts()
 
     # read in the bingo terms
-    in_file = open(sys.argv[1], 'r')
+    in_file = open(args['terms'], 'r')
     terms = [line.strip() for line in in_file.readlines()]
-    terms = list(filter(lambda x: x != "", terms))
     in_file.close()
+    terms = list(filter(lambda x: x != "", terms))
+    terms = ['"' + term + '"' for term in terms]
+    terms = ",".join(terms)
+    args['phrases'] = terms
 
     html_template = open("html_template.txt", "r").read()
-    out_file = open(sys.argv[2], 'w')
-    out_file.write(html_template)
-    cards = int(sys.argv[3])
-    for i in range(cards):
-        random.shuffle(terms)
-        if i != cards - 1:
-            out_file.write(generate_table(terms))
-        else:
-            out_file.write(generate_table(terms, False))
-    out_file.write("</body></html>")
+    for argname in ['phrases', 'color', 'heading']:
+        html_template = html_template.replace('{{' + argname + '}}', args[argname], 1)
 
-    out_file.close()
+    if args['output']:
+        out_file = open(args['output'], 'w')
+        out_file.write(html_template)
+        out_file.close()
+    else:
+        sys.stdout.write(html_template)
 
 
 if __name__ == '__main__':
